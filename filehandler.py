500f66fb1f00b4a343305b7dcde3f13b52305e82
Zuzanna <zuzanna.hertig@gmail.com>
2023-04-11 21:20:39 +0200

    feat: add file handler

---
+++ filehandler.py
@@ -0,0 +1,9 @@
+import json
+from typing import Dict
+
+
+class FileHandler:
+    @staticmethod
+    def append_to_file(file_name: str, data: Dict) -> None:
+        with open(f'{file_name}.json', 'a') as f:
+            json.dump(data, f)
