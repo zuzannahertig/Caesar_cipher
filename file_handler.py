51f981a7a87d61e2cc681b0608bc4563dc429950
Zuzanna <zuzanna.hertig@gmail.com>
2023-04-16 19:16:22 +0200

    feat: check if input data is correct

--- file_handler.py
+++ file_handler.py
@@ -8,8 +8,8 @@ class FileHandler:
     @staticmethod
     def write_to_file(file_name: str, data: List[Dict[str, ...]]) -> None:
         """Write data to JSON file."""
-        with open(f"{file_name}.json", "a") as f:
-            json.dump(data, f)
+        with open(f"{file_name}.json", "a") as file:
+            json.dump(data, file)
 
     @staticmethod
     def append_to_file(file_path: str, data: List[Dict[str, ...]]) -> None:
@@ -24,12 +24,19 @@ class FileHandler:
     @staticmethod
     def save_data() -> None:
         """Save information from current session in JSON file."""
-        file_name = input("Name of the file: ")
+        max_length = 50
+        file_name = input(f"Name of the file (max {max_length} characters): ")
+
+        while len(file_name) > max_length or len(file_name.strip()) == 0:
+            file_name = input(f"Invalid name. Enter new one: ")
+
         if os.path.exists(f"{file_name}.json"):
             FileHandler.append_to_file(
                 file_name, MemoryBuffer.return_buffer_as_dict(MemoryBuffer.memory)
             )
+            print(f"Data has been saved to existing file: {file_name}.json")
         else:
             FileHandler.write_to_file(
                 file_name, MemoryBuffer.return_buffer_as_dict(MemoryBuffer.memory)
             )
+            print(f"Data has been saved to new file: {file_name}.json")
